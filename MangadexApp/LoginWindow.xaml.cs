2165f6afec1bea487b243983338c76e37bb4abff
Kolos444 <4sukaLangley>
2022-02-18 11:55:28 +0100

    Begin der Arbeit am der Navigations Leiste

---
+++ MangadexApp/LoginWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using MangadexAPI;
+
+namespace MangadexApp;
+
+/// <summary>Interaction logic for LoginWindow.xaml</summary>
+public partial class LoginWindow : Window{
+	public LoginWindow() {
+		InitializeComponent();
+		}
+
+	private void LoginWindow_OnMouseDown(object sender, MouseButtonEventArgs e) {
+		if (e.ChangedButton == MouseButton.Left)
+			DragMove();
+	}
+
+
+	private void LoginButton_OnClick(object sender, RoutedEventArgs e) {
+		account = new Account(TextBox.Text, PasswordBox.Password);
+		Task<APIRequest?> answer = MangadexAPI.MangadexAPI.login(account);
+
+		if (answer.Result.result == "ok"){
+			account.token = answer.Result.token;
+			loginSuccess();
+		}
+		else
+			StatusBox.Text = answer.Result.result;
+	}
+
+	private void loginSuccess() {
+		
+	}
+
+	private MangadexAPI.Account account;
+}
--- MangadexApp/MainWindow.xaml.cs
+++ MangadexApp/MainWindow.xaml.cs
@@ -1,32 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using MangadexAPI;
-using System.Windows.Interop;
 
+namespace MangadexApp;
 
-namespace MangadexApp{
-	
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
-	public partial class MainWindow : Window{ 
+public partial class MainWindow : Window{
+	public MainWindow() {
+		InitializeComponent();
 		
-		public MainWindow() {
-			InitializeComponent();
-			
-		}
+	}
+	
+	private void MainWindow_OnMouseDown(object sender, MouseButtonEventArgs e) {
+		if (e.ChangedButton == MouseButton.Left)
+			DragMove();
+	}
+
+	private void MangaDropdow_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e) {
+		
 	}
 }
